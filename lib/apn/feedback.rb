b5d9b392ed06524c211c46612de88f71c4626840
Felipe Santos <felipegs@gmail.com>
2012-07-12 15:00:52 -0300

    correcao de feedback

--- lib/apn/feedback.rb
+++ lib/apn/feedback.rb
@@ -64,7 +64,7 @@ module APN
       setup_connection(@certificate_path)
 
       # Unpacking code borrowed from http://github.com/jpoz/APNS/blob/master/lib/apns/core.rb
-      while bunch = socket.read(38)   # Read data from the socket
+      while bunch = socket(@certificate_path).read(38)   # Read data from the socket
         f = bunch.strip.unpack('N1n1H140')
         feedback << APN::FeedbackItem.new(Time.at(f[0]), f[2])
       end
